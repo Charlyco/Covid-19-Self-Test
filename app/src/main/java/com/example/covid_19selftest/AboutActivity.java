be6111c46a92ce29bc71b59af23210c849b3e91d
Limitless <theyoungeng@gmail.com>
2020-10-09 23:04:18 +0100

    Some minor corrections and bug fixes

--- app/src/main/java/com/example/covid_19selftest/AboutActivity.java
+++ app/src/main/java/com/example/covid_19selftest/AboutActivity.java
@@ -1,6 +1,8 @@
 package com.example.covid_19selftest;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +12,13 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Toolbar cToolbar = findViewById(R.id.about_toolbar);
+        setSupportActionBar(cToolbar);
+
+        ActionBar cActionBar = getSupportActionBar();
+        if (cActionBar != null) {
+            cActionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
--- app/src/main/java/com/example/covid_19selftest/PrivacyPolicyActivity.java
+++ app/src/main/java/com/example/covid_19selftest/PrivacyPolicyActivity.java
@@ -1,6 +1,8 @@
 package com.example.covid_19selftest;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +12,13 @@ public class PrivacyPolicyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_policy);
+
+         Toolbar cToolbar = findViewById(R.id.privacy_toolbar);
+        setSupportActionBar(cToolbar);
+
+        ActionBar cActionBar = getSupportActionBar();
+        if (cActionBar != null) {
+            cActionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
